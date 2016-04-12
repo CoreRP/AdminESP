82dad64f73cb22f03db2e139e37fd671dcb035b9
Ralph <rama.o.ralph@gmail.com>
2016-04-11 23:34:27 -0400

    Refactored how text is drawn. GUI lookx very gud now

--- lua/autorun/client/cl_aesp.lua
+++ lua/autorun/client/cl_aesp.lua
@@ -1,13 +1,19 @@
+-- Localizing this stuff makes it go fast!
+local math      = math
+local string    = string
+local cam       = cam
+local table     = table
+
 AESP = AESP or {}
 AESP.Config = AESP.Config or {}
 
 -- Enable/disable the ESP
 AESP.Config.Enabled     = CreateClientConVar("aesp_enabled", "0" , false, false)
 -- Set the max draw distance
-AESP.Config.MaxDistance = CreateClientConVar("aesp_maxdistance", "16384", true, false)
+AESP.Config.MaxDistance = CreateClientConVar("aesp_maxdistance", "8192", true, false)
 
 -- Set the text offset in the y direction
-AESP.Config.TextOffsetY = CreateClientConVar("aesp_offsety", "50" , true, false)
+AESP.Config.TextOffsetY = CreateClientConVar("aesp_offsety", "85" , true, false)
 
 -- Draw the target's model, visible through the world
 AESP.Config.ShowModel   = CreateClientConVar("aesp_drawmodel", "0", true, false)
@@ -49,71 +55,125 @@ surface.CreateFont("aesp_font", {
 })
 
 local function DrawText(text, color, x, y)
-    draw.SimpleTextOutlined(text, "aesp_font", x, y, color, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, color_black)
+    local textWidth, _ = draw.SimpleTextOutlined(text, "aesp_font", x, y, color, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, color_black)
+    return textWidth
+end
+
+local function CalculateTextHeight()
+    local totalHeight = 0
+    local gamemodeName = string.lower(gmod.GetGamemode().Name)
+
+    if AESP.Config.ShowName:GetBool() then      totalHeight = totalHeight + 13 end
+    if AESP.Config.ShowSteamID:GetBool() then   totalHeight = totalHeight + 13 end
+    if AESP.Config.ShowPing:GetBool() then      totalHeight = totalHeight + 13 end
+    if AESP.Config.ShowHealth:GetBool() then    totalHeight = totalHeight + 13 end
+    if AESP.Config.ShowArmor:GetBool() then     totalHeight = totalHeight + 13 end
+    if AESP.Config.ShowGroup:GetBool() then     totalHeight = totalHeight + 13 end
+    if gamemodeName == "darkrp" then
+        if AESP.Config.ShowJob:GetBool() then   totalHeight = totalHeight + 13 end
+        if AESP.Config.ShowMoney:GetBool() then totalHeight = totalHeight + 13 end
+    end
+    if AESP.Config.ShowRole:GetBool() and gamemodeName == "trouble in terrorist town" then totalHeight = totalHeight + 13 end
+
+    if totalHeight ~= 0 then
+        return totalHeight
+    else
+        return false
+    end
+end
+
+local function CalculateMaxTextWidth(stringArray)
+    surface.SetFont("aesp_font")
+    local maxWidth = 0
+    
+    for _, checkString in pairs(stringArray) do
+        w, h = surface.GetTextSize(checkString)
+        if w > maxWidth then maxWidth = w end
+    end
+
+    -- change 6 to whatever number you want for nice padding
+    return maxWidth + 10
 end
 
 local function DrawTextESP(target)
+    -- Don't do anything if totalheight is false 
+    -- (i.e. none of the aesp_draw flags are enabled)
+    if not CalculateTextHeight() then return end
+    
     -- darkrp, terrortown, sandbox, murder, etc
     local gamemodeName = string.lower(gmod.GetGamemode().Name)
 
     -- Where to draw the text in relation to the target
     local textPosition = (target:GetPos() + Vector(0, 0, AESP.Config.TextOffsetY:GetInt())):ToScreen()
-    local nextLineOffset = 0
-    local textColor = Color(255, 255, 255, 255)
+    -- Calculate the height of all the enabled lines
+    local totalHeight = CalculateTextHeight()
+    -- line offset is incremented with each enabled flag so text doesn't overlap
+    local nextLineOffset = 1
+    -- the color of the text
+    local textColor = color_white
 
+    -- Create an array of every bit of text we'd like to draw
+    local infoArray = {}
+
+    -- insert all our strings into infoArray
     if AESP.Config.ShowName:GetBool() then
-        DrawText("Name: " .. target:Nick(), textColor, textPosition.x, textPosition.y + nextLineOffset)
-        nextLineOffset = nextLineOffset + 13
+        table.insert(infoArray, "Name: " .. target:Nick())
     end
-    
     if AESP.Config.ShowSteamID:GetBool() then
-        DrawText("SteamID: " .. target:SteamID(), textColor, textPosition.x, textPosition.y + nextLineOffset)
-        nextLineOffset = nextLineOffset + 13
+        table.insert(infoArray, "SteamID: " .. target:SteamID())
     end
-
     if AESP.Config.ShowPing:GetBool() then
-        DrawText("Ping: " .. target:Ping(), textColor, textPosition.x, textPosition.y + nextLineOffset)
-        nextLineOffset = nextLineOffset + 13
+        table.insert(infoArray, "Ping: " .. target:Ping())
     end
-
     if AESP.Config.ShowHealth:GetBool() then
-        DrawText("Health: " .. target:Health(), textColor, textPosition.x, textPosition.y + nextLineOffset)
-        nextLineOffset = nextLineOffset + 13
+        table.insert(infoArray, "Health: " .. target:Health())
     end
-
     if AESP.Config.ShowArmor:GetBool() then
-        DrawText("Armor: " .. target:Armor(), textColor, textPosition.x, textPosition.y + nextLineOffset)
-        nextLineOffset = nextLineOffset + 13
+        table.insert(infoArray, "Armor: " .. target:Armor())
     end
-
     if AESP.Config.ShowGroup:GetBool() then
-        DrawText("Usergroup: " .. target:GetNWString("usergroup"), textColor, textPosition.x, textPosition.y + nextLineOffset)
-        nextLineOffset = nextLineOffset + 13
+        table.insert(infoArray, "Usergroup: " .. target:GetNWString("usergroup"))
     end
-
-    if AESP.Config.ShowJob:GetBool() and gamemodeName == "darkrp" then
-        DrawText("Job: " .. team.GetName(target:Team()), textColor, textPosition.x, textPosition.y + nextLineOffset)
-        nextLineOffset = nextLineOffset + 13
-    end
-
-    if AESP.Config.ShowMoney:GetBool() and gamemodeName == "darkrp" then
-        DrawText("$" .. target:getDarkRPVar("money"), textColor, textPosition.x, textPosition.y + nextLineOffset)
-        nextLineOffset = nextLineOffset + 13
-    end
-
-    -- This doesn't work right now, shows Traitors as Innocents
-    -- Probably has to do with client-side shit, I'll figure it out later
-    if AESP.Config.ShowRole:GetBool() and gamemodeName == "trouble in terrorist town" then
-        local roleText = ""
-        if target:IsTraitor() then
-            roleText = "Traitor"
-        elseif target:IsDetective() then
-            roleText = "Detective"
-        else
-            roleText = "Innocent"
+    -- DarkRP-specific draw commands
+    if gamemodeName == "darkrp" then
+        if AESP.Config.ShowJob:GetBool() then
+            table.insert(infoArray, "Job: " .. team.GetName(target:Team()))
         end
-        
-        DrawText("Role:" .. roleText, textColor, textPosition.x, textPosition.y + nextLineOffset)
+        if AESP.Config.ShowMoney:GetBool() then
+            table.insert(infoArray, "$" .. target:getDarkRPVar("money"))
+        end
+    -- TTT-specific draw commands
+    elseif gamemodeName == "trouble in terrorst town" then
+        if AESP.Config.ShowRole:GetBool() then
+            -- Find the name of the team the target's on
+            local roleText = ""
+            if target:IsTraitor() then
+                roleText = "Traitor"
+            elseif target:IsDetective() then
+                roleText = "Detective"
+            else
+                roleText = "Innocent"
+            end
+            -- Insert the value
+            table.insert(infoArray, "Role:" .. roleText)
+        end
+    end
+
+    -- Find the maximum width (i.e. the width our background will be)
+    local maxWidth = CalculateMaxTextWidth(infoArray)
+
+    -- Create a background for our text so it doesn't get hard to read
+    local bgpanel = draw.RoundedBox(10,     -- Radius of corners
+        textPosition.x - (maxWidth / 2),    -- x position of panel
+        textPosition.y - totalHeight - 10,  -- y position of panel
+        maxWidth,                           -- width of panel
+        totalHeight + 10,                   -- height of panel
+        Color(76, 76, 76))                  -- panel background color
+
+    -- draw all our text!
+    for _, text in pairs(infoArray) do
+        DrawText(text, textColor, textPosition.x, textPosition.y - totalHeight + nextLineOffset)
+        nextLineOffset = nextLineOffset + 13
     end
 end
 
